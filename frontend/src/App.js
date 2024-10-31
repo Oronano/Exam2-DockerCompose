1b032ebb47e0bc0f5c31fcdc62f5fe643a2f8289
Andre <andre.coelho@hotmail.fr>
2024-10-31 11:26:33 +0100

    Add link front & back + CRUD

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,101 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [users, setUsers] = useState([]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const user = { email, password };
+    const API_URL = process.env.REACT_APP_API_URL/*  || "http://localhost:3000" */;
 
-    try {
-      const response = await fetch('http://urlback/api/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${API_URL}/api/users`);
+                setUsers(response.data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
 
-      if (response.ok) {
-        const data = await response.json();
-        console.log('User created:', data);
-      } else {
-        console.error('Failed to create user');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
+        fetchData();
+    }, [API_URL]);
 
-  return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const user = { email, password };
+
+        try {
+            const response = await axios.post(`${API_URL}/api/users`, user, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (response.status === 201) {
+                console.log("User created:", response.data);
+                setUsers([...users, response.data]);
+            } else {
+                console.error("Failed to create user");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            const response = await axios.delete(`${API_URL}/api/users/${id}`);
+            if (response.status === 200) {
+                setUsers(users.filter((user) => user.id !== id));
+            } else {
+                console.error("Failed to delete user");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
+    return (
+        <div className="App">
+            <div>
+                <h1>Users</h1>
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.id}>
+                            {user.email}
+                            <button onClick={() => handleDelete(user.id)}>
+                                Delete
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default App;
--- myapp-back/app.js
+++ myapp-back/app.js
@@ -1,41 +1,64 @@
-const express = require('express');
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
+const express = require("express");
+var cors = require("cors");
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
 
-dotenv.config({ path: '../backend.env' });
+dotenv.config({ path: "../docker/backend.env" });
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World, this is BackEnd!');
+app.get("/api/users", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM users");
+        res.status(200).json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
-app.post('/api/users', async (req, res) => {
-  const { email, password } = req.body;
+app.post("/api/users", async (req, res) => {
+    const { email, password } = req.body;
 
-  try {
-    const result = await pool.query(
-      'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *',
-      [email, password]
-    );
-    res.status(201).json(result.rows[0]);
-  } catch (error) {
-    console.error('Error inserting user:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+    try {
+        const result = await pool.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+            [email, password]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.delete("/api/users/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const result = await pool.query(
+            "DELETE FROM users WHERE id = $1 RETURNING *",
+            [id]
+        );
+        if (result.rowCount === 0) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
